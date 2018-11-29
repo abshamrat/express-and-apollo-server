ca3f47ffe56aff94f0a0b1e3ee64a3e132086cc5
shamrat17 <shamratakbar17@gmail.com>
2018-11-29 20:33:06 +0600

    conditions added

--- schema.js
+++ schema.js
@@ -1,6 +1,7 @@
 // https://medium.com/@sam.chai0501/graphql-converting-my-node-express-sequelize-postgresql-back-end-24232e535fb9
+// Schema help: https://join-monster.readthedocs.io/en/latest/batch-one-many/
 const { resolver } = require('graphql-sequelize');
-const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLNonNull, GraphQLList } = require('graphql')
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLBoolean, GraphQLNonNull, GraphQLList } = require('graphql')
 const { Todo, TodoItem } = require('./server/models');
 
 
@@ -17,6 +18,9 @@ const todoitemType = new GraphQLObjectType({
     content: {
       type: GraphQLString,
       description: 'The content of the item.',
+    },
+    complete: {
+      type: GraphQLBoolean
     }
   }
 })
@@ -37,7 +41,12 @@ const todoType = new GraphQLObjectType({
     },
     todoItem: {
       type: new GraphQLList(todoitemType),
-      description: 'todo items'
+      sqlBatch: {
+        // which column to match up to the users
+        thisKey: 'todoId',
+        // the other column to compare to
+        parentKey: 'id'
+      },
 
     }
   }
@@ -45,18 +54,56 @@ const todoType = new GraphQLObjectType({
 
 
 
-//schema
+//schema 
+// helper link: https://github.com/mickhansen/graphql-sequelize
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'root',
     fields: {
       todo: {
         type: new GraphQLList(todoType),
+         // args will automatically be mapped to `where`
+         args: {
+          id: {
+            description: 'id of the todo',
+            type: new GraphQLNonNull(GraphQLInt)
+          }
+        },
         resolve: resolver(Todo)
       },
       todoItem: {
         type: new GraphQLList(todoitemType),
-        resolve: resolver(TodoItem)
+         // args will automatically be mapped to `where`
+         args: {
+          query: {
+            description: "Fuzzy-matched name of user",
+            type: new GraphQLNonNull(GraphQLString),
+          }
+        },
+        resolve: resolver(TodoItem, {
+          // Custom `where` clause that fuzzy-matches user's name and
+          // alphabetical sort by username
+          before: (findOptions, args) => {
+            findOptions.where = {
+              content: { "$like": `%${args.query}%` },
+            };
+            findOptions.order = [['content', 'ASC']];
+            return findOptions;
+          },
+          // Custom sort override for exact matches first
+          after: (results, args) => {
+            return results.sort((a, b) => {
+              if (a.content === args.query) {
+                return 1;
+              }
+              else if (b.content === args.query) {
+                return -1;
+              }
+
+              return 0;
+            });
+          }
+        })
       }
     }
   })
